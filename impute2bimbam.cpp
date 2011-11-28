7ce54cb0dca6da57bd812d92ae6691bb0503639a
Tim Flutre <timflutre@example.com>
2011-11-28 16:56:51 -0600

    first commit

---
+++ impute2bimbam.cpp
@@ -0,0 +1,240 @@
+/** \file impute2bimbam.cpp
+ *
+ *  `impute2bimbam' converts genotype data format from IMPUTE into BIMBAM.
+ *  Copyright (C) 2011  T. Flutre
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  gcc -Wall impute2bimbam.cpp -lstdc++ -lgzstream -lz -o impute2bimbam
+ *  help2man -o impute2bimbam.man ./impute2bimbam
+ *  groff -mandoc impute2bimbam.man > impute2bimbam.ps
+*/
+
+#include <cmath>
+#include <ctime>
+#include <getopt.h>
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+#include "utils.cpp"
+
+//-----------------------------------------------------------------------------
+
+void help (char ** argv);
+void version (char ** argv);
+void parse_args (int argc, char ** argv,
+		 string * pt_input,
+		 string * pt_output,
+		 int * pt_verbose);
+void convertImputeFileToBimbamFile (string input,
+				    string output,
+				    int verbose);
+int main (int argc, char ** argv);
+
+//-----------------------------------------------------------------------------
+
+/** \brief Display the usage on stdout.
+*/
+void help (char ** argv)
+{
+  cout << "`" << argv[0] << "'"
+       << " converts genotype data format from IMPUTE into BIMBAM." << endl
+       << endl
+       << "Usage: " << argv[0] << " [OPTIONS]..." << endl
+       << endl
+       << "Options:" << endl
+       << "  -h, --help\tdisplay the help and exit" << endl
+       << "  -V, --version\toutput version information and exit" << endl
+       << "  -v, --verbose\tverbosity level (default=1)" << endl
+       << "  -i, --input\tpath to input directory and generic file name in the IMPUTE format" << endl
+       << "\t\teg. '~/data/chrXX_chunkAll.impute2'" << endl
+       << "  -o, --output\tgeneric name for the output files" << endl
+       << "\t\tdefault is 'chrXX.bimbam'" << endl
+       << endl
+       << "Examples:" << endl
+       << "  " << argv[0] << " -i ~/data/chrXX_chunkAll.impute2" << endl;
+}
+
+void version (char ** argv)
+{
+  cout << argv[0] << " 0.1" << endl
+       << endl
+       << "Copyright (C) 2011 T. Flutre." << endl
+       << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>" << endl
+       << "This is free software; see the source for copying conditions.  There is NO" << endl
+       << "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE." << endl
+       << endl
+       << "Written by T. Flutre." << endl;
+}
+
+/** \brief Parse the command-line arguments and check the values of the 
+ *  compulsory ones.
+ */
+void parse_args (int argc, char ** argv,
+		 string * pt_input,
+		 string * pt_output,
+		 int * pt_verbose)
+{
+  int c = 0;
+  while (1)
+  {
+    static struct option long_options[] =
+      {
+	{"help", no_argument, 0, 'h'},
+	{"version", no_argument, 0, 'V'},
+	{"verbose", required_argument, 0, 'v'},
+	{"input", required_argument, 0, 'i'},
+	{"output", required_argument, 0, 'o'},
+      };
+    int option_index = 0;
+    c = getopt_long (argc, argv, "hVv:i:o:",
+		     long_options, &option_index);
+    if (c == -1)
+      break;
+    switch (c)
+    {
+    case 0:
+      if (long_options[option_index].flag != 0)
+	break;
+      printf ("option %s", long_options[option_index].name);
+      if (optarg)
+	printf (" with arg %s", optarg);
+      printf ("\n");
+      break;
+    case 'h':
+      help (argv);
+      exit (0);
+    case 'V':
+      version (argv);
+      exit (0);
+    case 'v':
+      *pt_verbose = atoi(optarg);
+      break;
+    case 'i':
+      *pt_input = optarg;
+      break;
+    case 'o':
+      *pt_output = optarg;
+      break;
+    case '?':
+      break;
+    default:
+      abort ();
+    }
+  }
+  if ((*pt_input).empty())
+  {
+    fprintf (stderr, "ERROR: missing input (-i).\n");
+    help (argv);
+    exit (1);
+  }
+  if ((*pt_output).empty())
+  {
+    *pt_output = "chrXX.bimbam";
+  }
+}
+
+void convertImputeFilesToBimbamFiles (string input,
+				      string output,
+				      int verbose)
+{
+  string line;
+  ifstream inStream;
+  vector<string> tokens;
+  ofstream outStream;
+  size_t nbSamples = 0;
+  
+  for (int chrNb = 21; chrNb <= 22; ++chrNb)
+  {
+    if (verbose > 0)
+      cout << "convert genotypes on chr" << chrNb << "..." << endl;
+    
+    string inFile = copyString (input);
+    replaceAll (inFile, "XX", toString(chrNb));
+    inStream.open (inFile.c_str());
+    if (! inStream.is_open())
+    {
+      cerr << "ERROR: can't open file " << inFile << endl;
+      exit (1);
+    }
+    
+    string outFile = copyString (output);
+    replaceAll (outFile, "XX", toString(chrNb));
+    outStream.open (outFile.c_str());
+    if (! outStream.is_open())
+    {
+      cerr << "ERROR: can't open file " << outFile << endl;
+      exit (1);
+    }
+    
+    while (inStream.good())
+    {
+      getline (inStream, line);
+      if (line.empty())
+      {
+	break;
+      }
+      if (line.find('\t') != string::npos)
+	split (line, '\t', tokens);
+      else
+	split (line, ' ', tokens);
+      outStream << tokens[1]          // SNP id
+		<< " " << tokens[3]   // allele A (minor allele for BimBam)
+		<< " " << tokens[4];  // allele B (major allele for BimBam)
+      nbSamples = (size_t) floor ((tokens.size() - 5) / 3);
+      for (size_t i = 0; i < nbSamples; ++i)
+      {
+	outStream << " " << 2 * atof(tokens[5+3*i].c_str())
+	  + 1 * atof(tokens[5+3*i+1].c_str())
+	  + 0 * atof(tokens[5+3*i+2].c_str());
+      }
+      outStream << endl;
+    }
+    
+    inStream.close();
+    outStream.close();
+  }
+}
+
+int main (int argc, char ** argv)
+{
+  string input, output;
+  int verbose = 1;
+  parse_args (argc, argv, &input, &output, &verbose);
+  
+  time_t startRawTime, endRawTime;
+  if (verbose > 0)
+  {
+    time (&startRawTime);
+    cout << "START " << argv[0] << " (" << time2string (startRawTime) << ")" << endl;
+  }
+  
+  convertImputeFilesToBimbamFiles (input, output, verbose);
+  
+  if (verbose > 0)
+  {
+    time (&endRawTime);
+    cout << "END " << argv[0] << " (" << time2string (endRawTime)
+	 << ": elapsed -> " << elapsedTime(startRawTime, endRawTime)
+	 << ")" << endl;
+  }
+  
+  return 0;
+}
---
+++ utils.cpp
@@ -0,0 +1,219 @@
+/** \file utils.cpp
+ *
+ *  `utils.cpp' gathers functions useful for any programs.
+ *  Copyright (C) 2011  T. Flutre
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ctime>
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#include <gzstream.h>
+
+// http://stackoverflow.com/questions/1644868/c-define-macro-for-debug-printing/1644898#1644898
+#ifdef DEBUG
+#define DEBUG_TEST 1
+#else
+#define DEBUG_TEST 0
+#endif
+#define debug_print(fmt, ...)						\
+  do { if (DEBUG_TEST) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+
+/** \brief Split a string.
+ *  \note http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c/236803#236803
+ */
+vector<string> & split (const string & s, char delim, vector<string> & tokens)
+{
+  tokens.clear();
+  stringstream ss(s);
+  string item;
+  while(getline(ss, item, delim)) {
+    tokens.push_back(item);
+  }
+  return tokens;
+}
+
+/** \brief Split a string.
+ *  \note http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c/236803#236803
+ */
+vector<string> split (const string & s, char delim)
+{
+  vector<string> tokens;
+  return split (s, delim, tokens);
+}
+
+/** \brief Return a string with the elapsed time in hours, minutes and seconds.
+ */
+string elapsedTime (time_t startRawTime, time_t endRawTime)
+{
+  char str[128];
+  time_t elapsedSec = static_cast<time_t>(difftime (endRawTime, startRawTime));
+  struct tm * ptm;
+  ptm = gmtime (&elapsedSec);
+  sprintf (str, "%02ih %02im %02is",
+	   ptm->tm_hour,
+	   ptm->tm_min,
+	   ptm->tm_sec);
+  return string(str);
+}
+
+/** \brief Return a string with the given date-time, without end-of-line.
+ */
+string time2string (time_t inTime)
+{
+  char * ptr = ctime (&inTime);
+  char buffer[126];
+  strcpy (buffer, ptr);
+  buffer[strlen(buffer)-1] = 0;
+  return string(buffer);
+}
+
+/** \brief Load a one-column file compressed with gzip.
+ */
+vector<string> loadOneColumnFile (string inFile, int verbose)
+{
+  vector<string> vItems;
+  
+  if (inFile.empty())
+    return vItems;
+  
+  string line;
+  igzstream stream;
+  vector<string> tokens;
+  size_t line_id = 0;
+  
+  stream.open(inFile.c_str());
+  if (! stream.good())
+  {
+    cerr << "ERROR: can't open file " << inFile << endl;
+    exit (1);
+  }
+  if (verbose > 0)
+  {
+    cout <<"load file " << inFile << "..." << endl;
+  }
+  
+  while (stream.good())
+  {
+    getline (stream, line);
+    if (line.empty())
+    {
+      break;
+    }
+    line_id++;
+    split (line, '\t', tokens);
+    if (tokens.size() != 1)
+    {
+      cerr << "ERROR: file " << inFile << " should have only one column"
+	   << " at line " << line_id << endl;
+      exit (1);
+    }
+    if (find(vItems.begin(), vItems.end(), tokens[0]) == vItems.end())
+    {
+      vItems.push_back (tokens[0]);
+    }
+  }
+  
+  stream.close();
+  
+  if (verbose > 0)
+  {
+    cout << "items loaded: " << vItems.size() << endl;
+  }
+  
+  return vItems;
+}
+
+/** \brief Used by scandir.
+ */
+static int dummy_selector (const struct dirent * dir_entry)
+{
+  return 1;
+}
+
+/** \brief Return a vector with the iterations corresponding to nbSteps.
+ *  \note Useful with verbose to print at which iteration a loop is.
+ */
+vector<size_t> getCounters (size_t nbIterations, size_t nbSteps = 5)
+{
+  vector<size_t> vCounters;
+  size_t step = (size_t) floor (nbIterations / nbSteps);
+  for (size_t i = 1; i < nbSteps-1; ++i)
+    vCounters.push_back (i * step);
+  vCounters.push_back (nbIterations);
+  return vCounters;
+}
+
+/** \brief Print the nb of iterations already complete in percentage of
+ *  the total loop size.
+ */
+void printCounter (size_t currentIter, vector<size_t> vCounters)
+{
+  size_t i = 0;
+  while (i < vCounters.size())
+  {
+    if (currentIter == vCounters[i])
+    {
+      printf ("%.0f%%\n", (float) 100 * currentIter / vCounters[vCounters.size()-1]);
+      fflush (stdout);
+      break;
+    }
+    ++i;
+  } 
+}
+
+/** \brief Convert int, float, etc into a string.
+ *  \note http://notfaq.wordpress.com/2006/08/30/c-convert-int-to-string/
+ */
+template <class T>
+inline string toString (const T & t)
+{
+  stringstream ss;
+  ss << t;
+  return ss.str();
+}
+
+/** \brief Copy a string into another.
+ */
+string copyString (const string input)
+{
+  string output;
+  for (string::const_iterator it = input.begin();
+       it != input.end();
+       ++it)
+  {
+    output += *it;
+  }
+  return output;
+}
+
+/** \brief Replace part of a string with another string.
+ *  \note http://stackoverflow.com/a/3418285/597069
+ */
+void replaceAll (string & str, const string & from, const string & to)
+{
+  size_t start_pos = 0;
+  while((start_pos = str.find(from, start_pos)) != string::npos)
+  {
+    str.replace(start_pos, from.length(), to);
+    start_pos += to.length();  // in case 'to' contains 'from', eg. replacing 'x' with 'yx'
+  }
+}
