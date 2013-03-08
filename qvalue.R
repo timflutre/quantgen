f7ab19adc827d489fc3515bcbed95f9b3f9a6533
Timothee Flutre <timflutre@gmail.com>
2013-03-07 21:12:59 -0600

    plot hist p-values

--- qvalue.R
+++ qvalue.R
@@ -88,8 +88,6 @@ parseArgs <- function(){
       (length(params$columns) != 2) &&
       (length(params$columns) != 3)))
     stop("--col should specify 1, 2 or 3 columns", call.=FALSE)
-  if(! is.null(params$hist.file))
-    warning("--hist is not yet implemented", call.=FALSE)
   
   return(params)
 }
@@ -122,12 +120,13 @@ loadData <- function(file, header, columns, verbose=0){
 callSignifViaStoreyMethod <- function(data, fdr, verbose=0){
   if(verbose > 0)
     message("call significant tests via Storey's method ...")
+  set.seed(1859)
   qobj <- qvalue(p=data$pvalue, fdr.level=fdr, robust="TRUE",
                  pi0.method="bootstrap")
   if(verbose > 0){
     message(paste0("pFDR\t", fdr))
     message(paste0("pi0\t", format(qobj$pi0, digits=7)))
-    if(is.null(data$gene)){
+    if(! "gene" %in% names(data) || ! "snp" %in% names(data)){
       message(paste0("nbSignifTests\t", sum(qobj$significant)))
     } else{
       message(paste0("nbSignifGeneSnpPairs\t", sum(qobj$significant)))
@@ -159,9 +158,109 @@ saveRes <- function(data, qobj, file, verbose=0){
                                    FALSE, TRUE))
 }
 
-plotHistPvalues <- function(hist.file, verbose=0){
+getFdr <- function(pvalues=NULL, threshold=0.01){
+  stopifnot(! is.null(pvalues), length(pvalues) > 0)
+  res <- list()
+  set.seed(1859)
+  qobj <- qvalue(p=pvalues, robust="TRUE", pi0.method="bootstrap")
+  res$prop.true.null.tests <- qobj$pi0
+  nb.tests <- length(pvalues)
+  res$nb.signif.tests <- length(pvalues[pvalues <= threshold])
+  res$fdr <- (res$prop.true.null.tests * nb.tests * threshold) / res$nb.signif.tests
+  return(res)
+}
+
+plotHistPvalues <- function(data=NULL,
+                            maintitle=NULL,
+                            subtitle=NULL,
+                            xlab=NULL,
+                            ylab=NULL,
+                            writePi0=TRUE,
+                            ylim=NULL,
+                            file=NULL,
+                            verbose=0){
+  stopifnot(! is.null(data), length(data) > 0)
   if(verbose > 0)
     message("plot the histogram of p-values ...")
+  
+  if(! is.null(file))
+    pdf(file)
+  
+  if(! is.null(maintitle)){
+    if(! is.null(subtitle)){
+      par(mar=c(4.5, 4.1, 2, 0))
+    } else
+      par(mar=c(4.5, 4.1, 1, 0))
+  } else
+    par(mar=c(4.5, 4.1, 0, 0))
+  
+  tmp <- hist(data, breaks=99, col="darkgrey", border="white", main="",
+              xlab="", ylab="", yaxt="n", ylim=ylim)
+  if(! is.null(ylim)){
+    custom.at <- seq(from=0, to=ylim[2], length.out=5)
+    axis(side=2, at=custom.at, labels=TRUE, las=1)
+    step <- (custom.at[2] - custom.at[1]) / 5
+  } else{
+    axis(side=2, labels=TRUE, las=1)
+    step <- tmp$counts[1] / 10
+  }
+  if(is.null(xlab))
+    xlab <- expression(paste(italic(p),"-values"))
+  if(is.null(ylab))
+    ylab <- paste0("Number of tests (out of ", length(data), ")")
+  title(main=maintitle, xlab=xlab, ylab=ylab)
+  if (! is.null(subtitle)){
+    mtext(subtitle, line=-0.9)
+  }
+  
+  fdr <- getFdr(data)
+  pi0.perc <- sprintf("%.2f", 100 * fdr$prop.true.null.tests)
+  txt <- bquote(paste("proportion of null tests (", pi[0], "): ",
+                      .(pi0.perc), "%"))
+  if(writePi0){
+    if(is.null(ylim)){
+      text(x=0.5, y=floor(tmp$counts[1] - 1 * step), labels=txt)
+    } else{
+      text(x=0.5, y=custom.at[4] - 1 * step, labels=txt)
+    }
+  }
+  
+  fdrs <- c(0.05, 0.1, 0.15)
+  i <- 2
+  cex.fdr <- 1 # 0.8
+  tmp2 <- lapply(fdrs, function(fdr){
+    qres <- qvalue(p=data, fdr.level=fdr, robust=TRUE, pi0.method="bootstrap")
+    txt <- paste("FDR=", fdr, " : ",
+                 sum(qres$significant), " tests", sep="")
+    if(is.null(ylim)){
+      txt <- sprintf("FDR=%.2f", fdr)
+      text(x=0.33, y=floor(tmp$counts[1] - i * step), pos=4, labels=txt, cex=cex.fdr)
+      arrows(x0=0.51, y0=1.005 * floor(tmp$counts[1] - i * step),
+             x1=0.59, y1=1.005 * floor(tmp$counts[1] - i * step),
+             length=0.03, lwd=0.5)
+      txt <- paste0(sum(qres$significant), " tests")
+      text(x=0.6, y=floor(tmp$counts[1] - i * step), pos=4, labels=txt, cex=cex.fdr)
+    } else{
+      txt <- sprintf("FDR=%.2f", fdr)
+      text(x=0.33, y=custom.at[4] - i * step, pos=4, labels=txt, cex=cex.fdr)
+      arrows(x0=0.51, y0=custom.at[4] - i * step,
+             x1=0.59, y1=custom.at[4] - i * step,
+             length=0.03, lwd=0.5)
+      txt <- paste0(sum(qres$significant), " tests")
+      text(x=0.6, y=custom.at[4] - i * step, pos=4, labels=txt, cex=cex.fdr)
+    }
+    i <<- i + 1
+    qres
+  })
+  
+  if(! is.null(file))
+    dev.off()
+  
+  res <- list(pi0=fdr$prop.true.null.tests)
+  res[["fdr0.05"]] <- tmp2[[1]]
+  res[["fdr0.1"]] <- tmp2[[2]]
+  res[["fdr0.15"]] <- tmp2[[3]]
+  invisible(res)
 }
 
 run <- function(fdr=0.05, verbose=1){
@@ -174,7 +273,8 @@ run <- function(fdr=0.05, verbose=1){
   if(! is.null(params$out.file))
     saveRes(data, qobj, params$out.file, params$verbose)
   if(! is.null(params$hist.file))
-    plotHistPvalues(params$hist.file, params$verbose)
+    plotHistPvalues(data=data$pvalue, file=params$hist.file,
+                    verbose=params$verbose)
   if(params$verbose > 0)
     message(paste0("END ", prog.name, " (", date(), ")"))
 }
