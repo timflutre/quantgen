13465962fcd8e416f128411519a103c4c552ded9
Timothee Flutre <timflutre@gmail.com>
2013-10-11 16:32:31 -0500

    add binary.classif and hinton to utils_quantgen.R

--- utils_quantgen.R
+++ utils_quantgen.R
@@ -2,45 +2,145 @@
 ## Author: Timothee Flutre
 ## License: GPL-3
 
-## Random generation for the matrix-variate normal distribution
-## with mean equal to M, among-row covariance equal to U and
-## among-column covariance equal to V
+read.table.fast <- function(file, header, sep="", ...){
+  ## Read a large file as fast as possible
+  tmp <- read.table(file, header=header, nrows=5)
+  colClasses <- sapply(tmp, class)
+  if((tmp <- strsplit(p2f, "\\.")[[1]])[length(tmp)] == "gz"){
+    nb.lines <- as.numeric(system(paste0("zcat ", file, " | wc -l"), intern=TRUE))
+  } else
+    nb.lines <- as.numeric(system(paste0("wc -l < ", file), intern=TRUE))
+  read.table(file, header=header, nrows=nb.lines, colClasses=colClasses, sep=sep)
+}
+
+rmse <- function(error){
+  ## Return the Root Mean Squared Error
+  ##
+  ## Args:
+  ##  error: vector \hat{\theta}_i - \theta_i
+  sqrt(mean(error^2))
+}
+
+mae <- function(error){
+  ## Return the Mean Absolute Error
+  ##
+  ## Args:
+  ##  error: vector \hat{\theta}_i - \theta_i
+  mean(abs(error))
+}
+
+msd <- function(error){
+  ## Return the Mean Signed Difference
+  ##
+  ## Args:
+  ##  error: vector \hat{\theta}_i - \theta_i
+  mean(error)
+}
+
+binary.classif <- function(known.nulls, called.nulls){
+  ## Return the number of true positives, false positives, true negatives,
+  ## false negatives, true positive proportion (sensitivity), false positive
+  ## proportion, accuracy, true negative proportion (specificity), false
+  ## discovery proportion, false negative proportion and positive predictive
+  ## value (precision)
+  ##
+  ## Args:
+  ##  known.nulls: vector of booleans (TRUE if the null is true)
+  ##  called.nulls: vector of booleans (TRUE if the null is accepted)
+  ##
+  ## Notes:
+  ##  both vectors should be sorted beforehand
+  ##
+  ## Returns:
+  ##  vector with names
+  ##
+  ##                                  CALLED
+  ##                     Accepted null     Rejected null
+  ##
+  ##       true null         TN (U)            FP (V)          n0
+  ## TRUTH
+  ##       false null        FN (T)            TP (S)          n1
+  ##
+  ##                         a                 r               n
+  
+  stopifnot(is.vector(known.nulls), is.vector(called.nulls),
+            length(known.nulls) == length(called.nulls),
+            sum(! is.logical(known.nulls)) == 0,
+            sum(! is.logical(called.nulls)) == 0)
+  
+  n <- length(known.nulls) # total number of tests
+  n0 <- sum(known.nulls)   # nb of true nulls
+  n1 <- n - n0             # nb of "false nulls" (i.e. "true alternatives")
+  a <- sum(called.nulls)   # nb of accepted nulls ("called not significant")
+  r <- n - a               # nb of rejected nulls ("called significant", "discoveries")
+  
+  ## true positive = reject a false null
+  tp <- sum(which(! called.nulls) %in% which(! known.nulls))
+  
+  ## false positive = reject a true null (type I error, "false alarm")
+  fp <- sum(which(! called.nulls) %in% which(known.nulls))
+  
+  ## true negatives = accept a true null
+  tn <- sum(which(called.nulls) %in% which(known.nulls))
+  
+  ## false negatives = accept a false null (type II error, "miss")
+  fn <- sum(which(called.nulls) %in% which(! known.nulls))
+  
+  tpp <- tp / n1        # true positive prop (sensitivity)
+  fpp <- fp / n0        # false positive prop
+  acc <- (tp + tn) / n  # accuracy
+  tnp <- tn / n0        # true negative prop (specificity), = 1 - fpp
+  fdp <- fp / r         # false discovery prop
+  fnp <- fn / a         # false negative prop
+  ppv <- tp / r         # positive predictive value (precision)
+  
+  return(c(n=n, n0=n0, n1=n1, a=a, r=r,
+           tp=tp, fp=fp, tn=tn, fn=fn,
+           tpp=tpp, fpp=fpp, acc=acc, tnp=tnp, fdp=fdp, fnp=fnp, ppv=ppv))
+}
+
 rmatvnorm <- function(nrow, ncol, n, M=NULL, U=NULL, V=NULL){
+  ## Random generation for the matrix-variate normal distribution
+  ## with mean equal to M, among-row covariance equal to U and
+  ## among-column covariance equal to V
+  ## https://stat.ethz.ch/pipermail/r-help/2012-February/302442.html
+  ## http://en.wikipedia.org/wiki/Matrix_normal_distribution
   if(is.null(M))
     M <- matrix(data=0, nrow=nrow, ncol=ncol)
   if(is.null(U))
     U <- diag(nrow)
   if(is.null(V))
     V <- diag(ncol)
-  Z <- matrix(data=rnorm(n=nrow*ncol, mean=1, sd=1), nrow=nrow, ncol=ncol)
-  return(M + U %*% Z %*% V)
+  Z <- matrix(data=rnorm(n=nrow*ncol, mean=0, sd=1),
+              nrow=nrow, ncol=ncol)
+  return(M + sqrt(U) %*% Z %*% sqrt(V))
 }
 
-## Stable computation of log_{10}(\sum_i w_i 10^x_i)
-## use equal weights if not specified
 log10.weighted.sum <- function(x, weights=NULL){
+  ## Stable computation of log_{10}(\sum_i w_i 10^x_i)
+  ## use equal weights if not specified
   if(is.null(weights))
     weights <- rep(1/length(x), length(x))
   max <- max(x)
   max + log10(sum(weights * 10^(x - max)))
 }
 
-## Return the Moore-Penrose pseudo-inverse of a matrix
-mp.inv <- function()mat{
+mp.inv <- function(mat){
+  ## Return the Moore-Penrose pseudo-inverse of a matrix
   mat.svd <- svd(mat)
   mat.svd$v %*% diag(1/mat.svd$d) %*% t(mat.svd$u)
 }
 
-## GCT format: http://www.broadinstitute.org/cancer/software/genepattern/gp_guides/file-formats
 read.table.gct <- function(file=NULL){
+  ## GCT format: http://www.broadinstitute.org/cancer/software/genepattern/gp_guides/file-formats
   stopifnot(! is.null(file))
   tmp <- read.table(file, header=TRUE, row.names=1, skip=2)
   return(as.matrix(tmp[,-1], nrow=nrow(tmp), ncol=ncol(tmp)-1))
 }
 
-## Write with column names ("id" followed by sample names)
-## and row names (gene names)
 write.table.gct <- function(x=NULL, file=NULL, gzipped=TRUE){
+  ## Write with column names ("id" followed by sample names)
+  ## and row names (gene names)
   stopifnot(! is.null(x),
             ! is.null(rownames(x)),
             ! is.null(colnames(x)),
@@ -70,36 +170,12 @@ write.table.gct <- function(x=NULL, file=NULL, gzipped=TRUE){
                 col.names=FALSE, sep="\t")
 }
 
-## Transform gene expression levels into a N(0,1)
-## via quantile normalization (gene by gene, across samples)
-## 
-## mat: matrix with genes in rows and samples in columns
-## break.ties.rand: should the ties be broken randomly? yes by default
-## seed: to make it reproducible
-qqnorm.genexp <- function(mat, break.ties.rand=TRUE, seed=1859){
-  if(nrow(mat) < ncol(mat))
-    warning("input matrix doesn't seem to have genes in rows and samples in columns")
-  if(break.ties.rand && ! is.null(seed))
-    set.seed(seed)
-  
-  mat.qn <- t(apply(mat, 1, function(exp.per.gene){
-    if(break.ties.rand){
-      idx <- sample(length(exp.per.gene))
-      tmp <- qqnorm(exp.per.gene[idx], plot.it=FALSE)$x
-      tmp[sort(idx, index.return=TRUE)$ix]
-    } else
-      qqnorm(exp.per.gene, plot.it=FALSE)$x
-  }))
-  colnames(mat.qn) <- colnames(mat)
-  
-  return(mat.qn)
-}
-
-## Return the number of PCs that minimizes the average squared partial
-##  correlation (Shriner, Heredity, 2011)
-##
-## dat: genotype matrix (0,1,2) with SNPs in rows and individuals in columns
 getNbPCsMinimAvgSqPartCor <- function(X){
+  ## Return the number of PCs that minimizes the average squared partial
+  ##  correlation (Shriner, Heredity, 2011)
+  ##
+  ## Args:
+  ##  dat: genotype matrix (0,1,2) with SNPs in rows and individuals in columns
   if(nrow(X) < ncol(X))
     warning("input matrix doesn't seem to have genes/snps in rows and samples in columns")
   mu <- apply(X, 1, mean, na.rm=TRUE)
@@ -128,20 +204,21 @@ getNbPCsMinimAvgSqPartCor <- function(X){
   return(max(1, which.min(fm) - 1))
 }
 
-## Apply PCA on a gene expression matrix and identify
-## the leading principal components
-##
-## X: matrix with genes in rows and samples in columns
-## algo: can be 'svd' or 'eigen' (same results modulo a rotation)
-## method: to choose which PCs to remove, i.e. to interpret cutoff
-##         'nb': directly the nb of PCs to remove
-##         'pve': based on prop of variance explained
-##         'tw': based on p-value from Tracy-Widom test statistic
-##         'shriner': min the avg squared partial correlation
-## cutoff: to choose which PCs to remove, depends on 'method'
-## scree.file: file in which to save scree plot if non NULL
 pca.genexp <- function(X=NULL, algo="svd", method="nb", cutoff=10,
                        scree.file=NULL){
+  ## Apply PCA on a gene expression matrix and identify
+  ## the leading principal components
+  ##
+  ## Args:
+  ##  X: matrix with genes in rows and samples in columns
+  ##  algo: can be 'svd' or 'eigen' (same results modulo a rotation)
+  ##  method: to choose which PCs to remove, i.e. to interpret cutoff
+  ##          'nb': directly the nb of PCs to remove
+  ##          'pve': based on prop of variance explained
+  ##          'tw': based on p-value from Tracy-Widom test statistic
+  ##          'shriner': min the avg squared partial correlation
+  ##  cutoff: to choose which PCs to remove, depends on 'method'
+  ##  scree.file: file in which to save scree plot if non NULL
   stopifnot(! is.null(X), is.matrix(X),
             (algo %in% c("eigen","svd")),
             ! is.null(method), (method %in% c("nb","pve","tw")),
@@ -210,14 +287,15 @@ pca.genexp <- function(X=NULL, algo="svd", method="nb", cutoff=10,
   return(list(pcs=PCs, vars=e.vals, nb.pcs=nb.pcs))
 }
 
-## Return residuals of linear regressions used to remove a set
-## of confounders (e.g. PCs or PEER factors) from a matrix of
-## gene expression levels
-##
-## X: matrix with samples in rows and genes in columns
-##    (it will be centered and scaled before PCs are removed)
-## confounders: matrix with samples in rows and confounders in columns
 rm.confound.genexp <- function(X=NULL, confounders=NULL){
+  ## Return residuals of linear regressions used to remove a set
+  ## of confounders (e.g. PCs or PEER factors) from a matrix of
+  ## gene expression levels
+  ##
+  ## Args:
+  ##  X: matrix with samples in rows and genes in columns
+  ##     (it will be centered and scaled before PCs are removed)
+  ##  confounders: matrix with samples in rows and confounders in columns
   stopifnot(! is.null(X), is.matrix(X),
             ! is.null(confounders), is.matrix(confounders),
             nrow(X) == nrow(confounders))
@@ -228,12 +306,13 @@ rm.confound.genexp <- function(X=NULL, confounders=NULL){
   return(t(res$residuals))
 }
 
-## Impute missing expression levels per subgroup and per gene using the mean
-## (only genes expressed in all subgroups are considered)
-##
-## list.mat: list of matrices, one per subgroup with genes in rows
-##           and samples in columns
 imp.miss.genexp <- function(list.mat=NULL){
+  ## Impute missing expression levels per subgroup and per gene using the mean
+  ## (only genes expressed in all subgroups are considered)
+  ##
+  ## Args:
+  ##  list.mat: list of matrices, one per subgroup with genes in rows
+  ##            and samples in columns
   stopifnot(! is.null(list.mat), is.list(list.mat))
   for(subgroup in names(list.mat)){
     X <- list.mat[[subgroup]]
@@ -264,4 +343,79 @@ imp.miss.genexp <- function(list.mat=NULL){
     }))
     X.impM
   })
+}
+
+hinton <- function(m, main="", max.sqrt.m=NULL){
+  ## Plot a Hinton diagram
+  ##
+  ## Args:
+  ##  m: matrix
+  ##  main: main title (default="")
+  ##  max.sqrt.m: to play with the scaling
+  ##
+  ## Refs:
+  ##  modified from http://www.cs.princeton.edu/~mimno/factor-analysis.R
+  
+  rows <- dim(m)[1]
+  cols <- dim(m)[2]
+  
+  left <- rep(0, rows * cols)
+  right <- rep(0, rows * cols)
+  bottom <- rep(0, rows * cols)
+  top <- rep(0, rows * cols)
+  
+  box.colors <- rep("white", rows * cols)
+  
+  if(is.null(max.sqrt.m))
+    max.sqrt.m <- max(sqrt(abs(m)))
+  scale <- 0.9 / (2 * max.sqrt.m)
+  
+  position <- 1
+  
+  for(row in 1:rows){
+    for(col in 1:cols){
+      if(m[row,col] < 0)
+        box.colors[position] <- "black"
+      x <- sqrt(abs(m[row,col]))
+      left[position] <- col - (x * scale)
+      right[position] <- col + (x * scale)
+      top[position] <- -(row - (x * scale))
+      bottom[position] <- -(row + (x * scale))
+      position <- position + 1
+    }
+  }
+  
+  xlab <- ""
+  ylab <- ""
+  if(! is.null(names(dimnames(m)))){
+    xlab <- names(dimnames(m))[2]
+    ylab <- names(dimnames(m))[1]
+  }
+  
+  par(mar=c(ifelse(xlab == "", 1, 3),
+        ifelse(ylab == "", 1, 3), 5, 1) + 0.1)
+  
+  plot(0, xlim=c(0.25,cols+0.75), ylim=c(-rows-0.75, -0.25),
+       type="n", xaxt="n", yaxt="n", xlab="", ylab="")
+  
+  rect(left, bottom, right, top, col=box.colors)
+  
+  if(main != "")
+    title(main=main, line=3)
+  
+  if(! is.null(colnames(m))){
+    axis(side=3, at=1:cols, labels=FALSE)
+    text(x=1:cols, y=par("usr")[4] + 0.25, labels=colnames(m), adj=0, srt=45, xpd=TRUE)
+  } else
+    axis(side=3, at=1:cols, labels=1:cols)
+  
+  if(! is.null(rownames(m))){
+    axis(side=2, at=-(1:rows), labels=rownames(m), las=1)
+  } else
+    axis(side=2, at=-(1:rows), labels=1:rows, las=1)
+  
+  if(xlab != "")
+    mtext(xlab, side=1, line=1)
+  if(ylab != "")
+    mtext(ylab, side=2, line=2)
 }
