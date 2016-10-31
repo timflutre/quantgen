10f8e577e4808c194d088999557765117a7f7ec5
Timothée Flutre <timflutre@gmail.com>
2016-10-31 11:56:35 +0100

    first version of rmd2out.R

---
+++ rmd2out.R
@@ -0,0 +1,165 @@
+#!/usr/bin/env Rscript
+
+# Aim: convert an Rmd file into html or pdf
+# Copyright (C) 2016 Timothée Flutre
+# License: GPL-3+
+# Persons: Timothée Flutre [cre,aut]
+# Versioning: https://github.com/timflutre/quantgen
+
+rm(list=ls())
+prog.name <- "rmd2out.R"
+prog.version <- "0.1.0" # http://semver.org/
+
+R.v.maj <- as.numeric(R.version$major)
+R.v.min.1 <- as.numeric(strsplit(R.version$minor, "\\.")[[1]][1])
+if(R.v.maj < 2 || (R.v.maj == 2 && R.v.min.1 < 15))
+    stop("require R >= 2.15 (for paste0)", call.=FALSE)
+
+##' Display the help on stdout
+##'
+##' The format complies with help2man (http://www.gnu.org/s/help2man) but use --no-discard-stderr
+##' @title Help
+help <- function(){
+  txt <- paste0("`", prog.name, "' converts an Rmd file into html or pdf.\n")
+  txt <- paste0(txt, "\n")
+  txt <- paste0(txt, "Usage: ", prog.name, " [OPTIONS] ...\n")
+  txt <- paste0(txt, "\n")
+  txt <- paste0(txt, "Options:\n")
+  txt <- paste0(txt, "  -h, --help\tdisplay the help and exit\n")
+  txt <- paste0(txt, "  -V, --version\toutput version information and exit\n")
+  txt <- paste0(txt, "  -v, --verbose\tverbosity level (0/default=1/2/3)\n")
+  txt <- paste0(txt, "  -i, --input\tpath to the input file (Rmd format)\n")
+  txt <- paste0(txt, "  -o, --output\toutput format (default=html/pdf)\n")
+  txt <- paste0(txt, "\n")
+  txt <- paste0(txt, "Examples:\n")
+  txt <- paste0(txt, "  ", prog.name, " -i myreport.Rmd -o html\n")
+  txt <- paste0(txt, "\n")
+  txt <- paste0(txt, "Report bugs to <timothee.flutre@supagro.inra.fr>.")
+  write(txt, stdout())
+}
+
+##' Display version and license information on stdout
+##'
+##' The person roles comply with R's guidelines (The R Journal Vol. 4/1, June 2012). To comply with help2man (http://www.gnu.org/s/help2man), use --no-discard-stderr.
+##' @title Version
+version <- function(){
+  txt <- paste0(prog.name, " ", prog.version, "\n")
+  txt <- paste0(txt, "\n")
+  txt <- paste0(txt, "Copyright (C) 2016 Institut national de la recherche agronomique.\n")
+  txt <- paste0(txt, "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n")
+  txt <- paste0(txt, "\n")
+  txt <- paste0(txt, "Written by Timothée Flutre [cre,aut].")
+  write(txt, stdout())
+}
+
+##' Parse the command-line arguments
+##'
+##' Allow short and long options
+##' @title Command-line
+##' @param params list of parameters initialized with default values
+##' @return List of parameters
+parseCmdLine <- function(params){
+  args <- commandArgs(trailingOnly=TRUE)
+  ## print(args)
+
+  i <- 0
+  while(i < length(args)){ # use "while" loop for options with no argument
+    i <- i + 1
+    if(args[i] == "-h" || args[i] == "--help"){
+      help()
+      quit("no", status=0)
+    }
+    else if(args[i] == "-V" || args[i] == "--version"){
+      version()
+      quit("no", status=0)
+    }
+    else if(args[i] == "-v" || args[i] == "--verbose"){
+      params$verbose <- as.numeric(args[i+1])
+      i <- i + 1
+    }
+    else if(args[i] == "-i" || args[i] == "--input"){
+      params$in.file <- args[i+1]
+      i <- i + 1
+    }
+    else if(args[i] == "-o" || args[i] == "--output"){
+      params$out.format <- args[i+1]
+      i <- i + 1
+    }
+    else{
+      write(paste0(prog.name, ": invalid option -- ", args[i], "\n"), stderr())
+      help()
+      quit("no", status=1)
+    }
+  }
+
+  return(params)
+}
+
+##' Check the values of the command-line parameters
+##'
+##' @param params list of parameters
+checkParams <- function(params){
+  if(is.null(params$in.file)){
+    write("ERROR: missing compulsory option --input\n", stderr())
+    help()
+    quit("no", status=1)
+  }
+  if(! file.exists(params$in.file)){
+    write(paste0("ERROR: can't find file ", params$in.file, "\n"), stderr())
+    help()
+    quit("no", status=1)
+  }
+  if(! params$out.format %in% c("html", "pdf")){
+    write(paste0("ERROR: unrecognized output format ", params$out.format, "\n"),
+          stderr())
+    help()
+    quit("no", status=1)
+  }
+  library(rmarkdown)
+}
+
+run <- function(params){
+  rmarkdown::render(params$in.file, paste0(params$out.format, "_document"))
+}
+
+main <- function(){
+  params <- list(verbose=1,
+                 in.file=NULL,
+                 out.format="html")
+
+  params <- parseCmdLine(params)
+
+  checkParams(params)
+
+  if(params$verbose > 0){
+    start.time <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+    write(paste0("START ", prog.name, " ", prog.version, " ", start.time),
+          stdout())
+    args <- commandArgs(trailingOnly=TRUE)
+    write(paste("cmd-line:", prog.name, paste(args, collapse=" ")), stdout())
+    write(paste0("cwd: ", getwd()), stdout())
+  }
+
+  system.time(run(params))
+
+  if(params$verbose > 0){
+    end.time <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+    difft <- as.numeric(
+        difftime(as.POSIXct(end.time, format="%Y-%m-%d %H:%M:%S"),
+                 as.POSIXct(start.time, format="%Y-%m-%d %H:%M:%S"),
+                 units="days"))
+    ## difft <- 1 + 2/24 + 3/(24*60) + 3/(24*3600) # 1d 2h 3m 4s in days
+    difft.d <- floor(difft)
+    difft.h <- floor(((difft - difft.d) * 24) %% 24)
+    difft.m <- floor(((difft - difft.d - difft.h/24) * 24*60) %% (24 * 60))
+    difft.s <- floor(((difft - difft.d - difft.h/24 - difft.m/(24*60)) *
+                      24*60*60) %% (24 * 60 * 60))
+    run.length <- sprintf("%02i:%02i:%02i", difft.h, difft.m, difft.s)
+    write(paste0("END ", prog.name, " ", prog.version, " ", end.time,
+                 " (", run.length, ")"), stdout())
+    ## print(object.size(x=lapply(ls(), get)), units="Kb") # return an error I don't understand
+  }
+}
+
+if(! interactive())
+    main()
